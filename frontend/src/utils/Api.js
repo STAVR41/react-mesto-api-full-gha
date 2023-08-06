198b109fb058f381fad26111f8112c6075cd4f9e
Pavel Shershnev <pavelsersnev@gmail.com>
2023-08-06 15:27:31 +1200

    fix-api

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -75,7 +75,7 @@ class Api {
 
 }
 const api = new Api({
-    baseUrl: 'http://localhost:4000',
+    baseUrl: "http://api.stavr.nomoreparties.co",
     headers: {
         'Content-Type': 'application/json'
     }
