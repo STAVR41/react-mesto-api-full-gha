066cd7f2d185c8ed7d0510e2e917fbdadda05f96
Pavel Shershnev <pavelsersnev@gmail.com>
2023-08-07 22:33:53 +1200

    s15-fix-cors-login

--- backend/controllers/login.js
+++ backend/controllers/login.js
@@ -1,11 +1,14 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
+const { JWT_SECRET_KEY } = require('../utils/constants');
+
+const { NODE_ENV, JWT_SECRET } = process.env;
 
 function authentication(req, res, next) {
   const { password, email } = req.body;
   return User.findUserByEmail(email, password)
     .then((login) => {
-      const token = jwt.sign({ _id: login._id }, 'some-secret-key', { expiresIn: '7d' });
+      const token = jwt.sign({ _id: login._id }, NODE_ENV === 'production' ? JWT_SECRET : JWT_SECRET_KEY, { expiresIn: '7d' });
       res.cookie('jwt', token, {
         maxAge: 604800,
         httpOnly: true,
